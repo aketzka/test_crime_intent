08d8fd34ef4f5e2514d728d0167804903c2f3cdd
Yakovlev Alexander <yakovlev.alexander.work@gmail.com>
2018-01-13 12:02:11 +0300

    chapter 12 done

---
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/DatePickerActivity.java
@@ -0,0 +1,8 @@
+package criminalintent.android.bignerdranch.com.criminalintent;
+
+/**
+ * Created by User on 07.01.2018.
+ */
+
+public class DatePickerActivity extends SingleFragmentActivity {
+}
---
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/TimePickerFragment.java
@@ -0,0 +1,10 @@
+package criminalintent.android.bignerdranch.com.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by User on 07.01.2018.
+ */
+
+public class TimePickerFragment extends Fragment {
+}
